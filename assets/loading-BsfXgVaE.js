90ad24b836386a067671dcca380f27a0f0ee06f0
陈敬起 <35548612+GHchenjingqi@users.noreply.github.com>
2024-11-22 14:32:40 +0800

    1.0.4
    修复loading值大于100%

--- assets/loading-BsfXgVaE.js
+++
@@ -1,1 +0,0 @@
-import{C as a}from"./index-D9t0if1H.js";const o=a("loading",{state:()=>({loadingStatus:!1}),getters:{getLoadingStatus:t=>t.loadingStatus},actions:{setLoadingStatus(t){this.loadingStatus=t}}});export{o as u};
