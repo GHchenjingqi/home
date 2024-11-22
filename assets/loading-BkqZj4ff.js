5559f3565ad2adb01ff2639221703adb71af7e20
陈敬起 <35548612+GHchenjingqi@users.noreply.github.com>
2024-11-22 14:48:07 +0800

    1.0.5

--- assets/loading-BkqZj4ff.js
+++
@@ -1,1 +0,0 @@
-import{C as a}from"./index-DkSijEpZ.js";const o=a("loading",{state:()=>({loadingStatus:!1}),getters:{getLoadingStatus:t=>t.loadingStatus},actions:{setLoadingStatus(t){this.loadingStatus=t}}});export{o as u};
