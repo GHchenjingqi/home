800fc2de407829cac889f5a350ffeec7be0a6945
陈敬起 <35548612+GHchenjingqi@users.noreply.github.com>
2024-12-14 13:37:54 +0800

    1.2.1
    开启gzip压缩

---
+++ assets/loading-CMOJuFDU.js
@@ -0,0 +1,1 @@
+import{D as a}from"./index-CyIOnBVb.js";const o=a("loading",{state:()=>({loadingStatus:!1}),getters:{getLoadingStatus:t=>t.loadingStatus},actions:{setLoadingStatus(t){this.loadingStatus=t}}});export{o as u};
