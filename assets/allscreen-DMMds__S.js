c858442a10dbd3d6cbe0f740471d56c50f3588ab
陈敬起 <35548612+GHchenjingqi@users.noreply.github.com>
2025-07-29 15:41:51 +0800

    1.5.0.2

--- assets/allscreen-DMMds__S.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./index-3gd7oj85.js";const s=e("allscreen",{state:()=>({flag:!1}),getters:{getFlagStatus:t=>t.flag},actions:{setFlagStatus(t){this.flag=t}}}),l=e("codeStore",{state:()=>({flag:!1}),getters:{getFlagStatus:t=>t.flag},actions:{setFlagStatus(t){this.flag=t}}});export{l as a,s as u};
