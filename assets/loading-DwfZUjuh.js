77317b9f48098ce56b2cda39713f7364372a59de
陈敬起 <35548612+GHchenjingqi@users.noreply.github.com>
2025-08-04 09:36:06 +0800

    1.5.2.3

--- assets/loading-DwfZUjuh.js
+++
@@ -1,1 +0,0 @@
-import{q as a}from"./index-C8lrHyf0.js";const o=a("loading",{state:()=>({loadingStatus:!1}),getters:{getLoadingStatus:t=>t.loadingStatus},actions:{setLoadingStatus(t){this.loadingStatus=t}}});export{o as u};
